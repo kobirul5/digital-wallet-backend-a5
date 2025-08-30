b16e31ebc1995c6ee972f78e4e6b1855726686e1
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-08-30 18:45:16 +0600

    complete basic stucture and complet auth and user part

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,22 @@
-
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
-
+import { envVars } from "./app/config/env";
+import { connectRedis } from "./app/config/redis.config";
+import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 
 let server: Server;
 
 
 const startServer = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL!);
+        await mongoose.connect(envVars.DB_URL)
 
         console.log("Connected to DB!!");
 
-        server = app.listen(5000, () => {
-            console.log(`Server is listening to port ${5000}`);
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is listening to port ${envVars.PORT}`);
         });
     } catch (error) {
         console.log(error);
@@ -24,9 +24,58 @@ const startServer = async () => {
 }
 
 (async () => {
+    await connectRedis()
     await startServer()
+    await seedSuperAdmin()
 })()
 
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal recieved... Server shutting down..");
 
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal recieved... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught Exception detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
 
 
